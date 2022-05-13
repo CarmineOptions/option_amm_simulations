76db91a2e625b0438af20a01cb8551e3da3b8730
Marek Hauzr <marek.hauzr@gmail.com>
2022-05-13 12:22:11 +0200

    test correct Option initialization

--- simulations/amm.py
+++ simulations/amm.py
@@ -3,6 +3,8 @@ import math
 
 import scipy.stats
 
+from simulations.option import Option
+
 
 def black_scholes(vol: float, s: float, k: float, r: float, t: int) -> Tuple[float, float]:
     d_1 = 1 / math.sqrt(t) / vol * (math.log(s / k) + (r + vol ** 2 / 2) * t)
@@ -18,43 +20,6 @@ def black_scholes(vol: float, s: float, k: float, r: float, t: int) -> Tuple[flo
 
 class NotEnoughPoolCapitalError(Exception):
     pass
-
-
-class Option:
-    def __init__(
-            self,
-            strike_price: float,
-            type_: str,
-            long_short: str,
-            locked_capital: float,
-            quantity: float
-    ) -> None:
-        self.strike_price = strike_price
-        if type_ not in {'call', 'put'}:
-            raise ValueError
-        self.type_ = type_
-        if long_short not in {'long', 'short'}:
-            raise ValueError
-        self.long_short = long_short
-        self.locked_capital = locked_capital
-        self.quantity = quantity
-
-    def __dict__(self) -> Dict[str, Any]:
-        return {
-            'strike_price': self.strike_price,
-            'type_': self.type_,
-            'long_short': self.long_short,
-            'locked_capital': self.locked_capital,
-            'quantity': self.quantity
-        }
-    def __str__(self) -> str:
-        return f"""
-            strike_price: {self.strike_price}
-            type_: {self.type_}
-            long_short: {self.long_short}
-            locked_capital: {self.locked_capital}
-            quantity: {self.quantity}
-        """
 
 
 class AMM:
